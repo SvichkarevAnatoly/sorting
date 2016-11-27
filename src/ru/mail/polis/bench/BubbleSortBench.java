caab349c89b0cfe5ef6372e6708270e47d9a08e5
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-11-27 14:22:55 +0300

    refactor: do not use blackholes

--- src/ru/mail/polis/bench/BubbleSortBench.java
+++ src/ru/mail/polis/bench/BubbleSortBench.java
@@ -1,23 +1,14 @@
 package ru.mail.polis.bench;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-
 import ru.mail.polis.sort.BubbleSort;
 import ru.mail.polis.sort.Helper;
+
+import java.util.concurrent.TimeUnit;
 
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
@@ -31,9 +22,14 @@ public class BubbleSortBench {
         a = Helper.gen(1000);
     }
 
+    /*
+    * NOTE: If you are only producing a single result, it is more readable to
+    * use the implicit return. Do not make your benchmark
+    * code less readable with explicit Blackholes!
+    */
     @Benchmark
-    public void measureBubbleSort(Blackhole bh) {
-        bh.consume(BubbleSort.sort(a));
+    public int[] measureBubbleSort() {
+        return BubbleSort.sort(a);
     }
 
     public static void main(String[] args) throws RunnerException {
